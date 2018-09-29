b851cbc35062061cbb5baf5d3851baa8adf123e0
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2018-09-28 20:31:13 -0500

    Update manim_texto.py

--- manim_texto.py
+++ manim_texto.py
@@ -1,159 +1,121 @@
 from big_ol_pile_of_manim_imports import *
-#RENOMBRAR: Formato_textos
-class Animacion1(Scene): #Este es un comentario, no es código
-    def construct(self): #Este es un comentario, no es código
-        texto = TextMobject(
-            """Esto es texto normal, $esto es una formula$
-            $$esto es una formula$$"""
-        )
+
+class Texto(Scene): 
+    def construct(self): 
+        texto = TextMobject("Esto es un texto normal")
         self.play(Write(texto))
         self.wait(3)
 
-class Animacion2(Scene):
-    def construct(self):
-        texto = TextMobject(
-            """Esto es texto normal, $\\frac{x}{y}$
-            $$x^2+y^2=a^2$$"""
-        )
-        self.play(Write(texto))
+class Formula(Scene): 
+    def construct(self): 
+        formula = TexMobject("Esto es una formula")
+        self.play(Write(formula))
         self.wait(3)
 
-class Animacion3(Scene):
-    def construct(self):
-        texto = TextMobject(
-            """Esto es texto normal, $\\displaystyle\\frac{x}{y}$
-            $$x^2+y^2=a^2$$"""
-        )
-        self.play(Write(texto))
+class TextoMixto(Scene): 
+    def construct(self): 
+        textoMixto = TextMobject("""
+        	Esto es un texto normal,
+        	$esto es una formula$,
+        	$$esto es una formula$$
+        	""")
+        self.play(Write(textoMixto))
         self.wait(3)
 
-class Animacion4(Scene): #usando add
-    def construct(self):
-        texto = TextMobject(
-            """Esto es texto normal, $\\displaystyle\\frac{x}{y}$
-            $$x^2+y^2=a^2$$"""
-        )
-        self.add(texto)
+class TextoMixto2(Scene): 
+    def construct(self): 
+        textoMixto = TextMobject("""
+        	Esto es un texto normal,
+        	$\\frac{x}{y}$,
+        	$$x^2+y^2=a^2$$
+        	""")
+        self.play(Write(textoMixto))
         self.wait(3)
 
-class Formula(Scene):
-	def construct(self):
-		tex = TexMobject("\\sqrt{\\frac{x}{y}}")
-		self.play(Write(tex))
-		self.wait(3)
+class TextoMixtoDisplay(Scene): 
+    def construct(self): 
+        textoMixto = TextMobject("""
+        	Esto es un texto normal,
+        	$\\displaystyle\\frac{x}{y}$,
+        	$$x^2+y^2=a^2$$
+        	""")
+        self.play(Write(textoMixto))
+        self.wait(3)
 
-class PosicionamientoTextoCentro(Scene):
+class PosicionTextoCentro(Scene):
     def construct(self):
         texto = TextMobject("Texto genérico.")
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoSuperior(Scene):
+class PosicionTextoSuperior(Scene):
     def construct(self):
         texto = TextMobject("Texto genérico.")
         texto.to_edge(UP)
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoInferior(Scene):
+class PosicionTextoInferior(Scene):
     def construct(self):
         texto = TextMobject("Texto genérico.")
         texto.to_edge(DOWN)
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoDerecha(Scene):
+class PosicionTextoDerecha(Scene): #Probar con "Texto genérico ampliado"
     def construct(self):
-        texto = TextMobject("Texto genérico ampliado.")
+        texto = TextMobject("Texto genérico.")
         texto.to_edge(RIGHT)
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoIzquierda(Scene):
+class PosicionTextoIzquierda(Scene):
     def construct(self):
         texto = TextMobject("Texto genérico.")
         texto.to_edge(LEFT)
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoSuperiorDerecha(Scene):
+class PosicionTextoSuperiorDerecha(Scene):
     def construct(self):
         texto = TextMobject("Texto genérico.")
         texto.to_edge(UP+RIGHT)
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoInferiorIzquierda(Scene): 
+class PosicionTextoInferiorIzquierda(Scene): 
     def construct(self): #Este es un comentario, no es código
         texto = TextMobject("Texto genérico.") #Este es un comentario, no es código
         texto.to_edge(LEFT+DOWN)
         self.play(Write(texto))
         self.wait(3)
 
-class PosicionamientoTextoEspecial1(Scene): #introdiccion con move_to
+class PosicionPersonalizado1(Scene):
     def construct(self):
         textoM = TextMobject("Texto genérico.")
-        textoC = TextMobject("Texto genérico.")
-        textoM.move_to(0.25*UP)
+        textoC = TextMobject("Texto genérico central.") #Texto de referencia
+        textoM.move_to(0.25*UP)  #Jugar con los valores numéricos
         self.play(Write(textoM),Write(textoC))
         self.wait(3)
 
-class PosicionamientoTextoEspecial2(Scene): #move_to siempre es referente al centro
+class PosicionPersonalizado2(Scene): #move_to siempre es referente al centro
     def construct(self):
         textoM = TextMobject("Texto genérico.")
-        textoC = TextMobject("Texto genérico.")
-        textoM.move_to(0.5*UP+0.5*RIGHT)
+        textoC = TextMobject("Texto genérico central.")
+        textoM.move_to(1*UP+1*RIGHT)
         self.play(Write(textoM),Write(textoC))
         self.wait(1)
         textoM.move_to(1*UP+1*RIGHT) #El sistema de referencia es el centro
         self.play(Write(textoM))
+        self.wait(3)
 
-class PosicionamientoTextoEspecial3v1(Scene): 
+class PosicionRelativa1(Scene):
     def construct(self):
-    	#Escribe primero 
-        textoM = TextMobject("Texto genérico.")
-        textoC = TextMobject("Texto genérico.")
-        textoC.move_to(1*UP+1*RIGHT)
-        textoM.move_to(1*UP+1*RIGHT)
-        self.play(Write(textoM),Write(textoC))
-        self.wait(1)
-
-class PosicionamientoTextoEspecial3v2(Scene): #usando next_to,posicionamiento relativo con move_to
-    def construct(self):
-    	#Escribe primero 
         textoM = TextMobject("Texto relativo.")
         textoC = TextMobject("Texto de referencia.")
-        textoC.move_to(1*UP+1*RIGHT)
-        textoM.next_to(textoC,LEFT,buff=1) #jugar con los valores de buff y posición
+        textoM.next_to(textoC,LEFT,buff=1) #La posición toma como referencia la posición de textoC
         self.play(Write(textoM),Write(textoC))
-        self.wait(1)
-
-class PosicionamientoTextoEspecial3v3(Scene): #usando next_to con to_edge
-    def construct(self):
-    	#Escribe primero 
-        textoM = TextMobject("Texto relativo.")
-        textoC = TextMobject("Texto de referencia.")
-        textoC.to_edge(UP+RIGHT)
-        textoM.next_to(textoC,DOWN,buff=1) #jugar con los valores de buff y posición
-        self.play(Write(textoM),Write(textoC))
-        self.wait(1)
-
-class Fonts(Scene):
-	def construct(self):
-		textoNormal = TextMobject("{Texto normal 012.\\#!?} Texto normal")
-		textoItalica = TextMobject("\\textit{Texto en itálicas 012.\\#!?} Texto normal")
-		textoMaquina = TextMobject("\\texttt{Texto en máquina 012.\\#!?} Texto normal")
-		textoNegritas = TextMobject("\\textbf{Texto en negritas 012.\\#!?} Texto normal")
-		textoSL = TextMobject("\\textsl{Texto en sl 012.\\#!?} Texto normal")
-		textoSC = TextMobject("\\textsc{Texto en sc 012.\\#!?} Texto normal")
-		textoNormal.to_edge(UP)
-		textoItalica.next_to(textoNormal,DOWN,buff=.5)
-		textoMaquina.next_to(textoItalica,DOWN,buff=.5)
-		textoNegritas.next_to(textoMaquina,DOWN,buff=.5)
-		textoSL.next_to(textoNegritas,DOWN,buff=.5)
-		textoSC.next_to(textoSL,DOWN,buff=.5)
-		self.add(textoNormal,textoItalica,textoMaquina,textoNegritas,textoSL,textoSC)
-		self.wait(3)
+        self.wait(3)
 
 class Tamanhos(Scene):
 	def construct(self):
@@ -186,8 +148,19 @@ class TamanhosPersonalizados(Scene):
 		self.play(Write(texto))
 		self.wait(3)
 
-class TextoColores(Scene):
+class Fuentes(Scene):
 	def construct(self):
-		texto = TextMobject("{\\color{red} Rojo}")
-		self.play(Write(texto))
+		textoNormal = TextMobject("{Texto normal 012.\\#!?} Texto normal")
+		textoItalica = TextMobject("\\textit{Texto en itálicas 012.\\#!?} Texto normal")
+		textoMaquina = TextMobject("\\texttt{Texto en máquina 012.\\#!?} Texto normal")
+		textoNegritas = TextMobject("\\textbf{Texto en negritas 012.\\#!?} Texto normal")
+		textoSL = TextMobject("\\textsl{Texto en sl 012.\\#!?} Texto normal")
+		textoSC = TextMobject("\\textsc{Texto en sc 012.\\#!?} Texto normal")
+		textoNormal.to_edge(UP)
+		textoItalica.next_to(textoNormal,DOWN,buff=.5)
+		textoMaquina.next_to(textoItalica,DOWN,buff=.5)
+		textoNegritas.next_to(textoMaquina,DOWN,buff=.5)
+		textoSL.next_to(textoNegritas,DOWN,buff=.5)
+		textoSC.next_to(textoSL,DOWN,buff=.5)
+		self.add(textoNormal,textoItalica,textoMaquina,textoNegritas,textoSL,textoSC)
 		self.wait(3)
